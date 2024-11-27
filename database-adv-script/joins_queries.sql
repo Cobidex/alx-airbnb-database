191f6a810b1c6cccf8b99bdf3df5cc33d9e109b3
Stephen <stevobialor@gmail.com~>
2024-11-27 15:20:56 +0100

    order by property id

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,7 +2,8 @@ SELECT * FROM Bookings
 INNER JOIN Users ON Bookings.user_id = Users.id;
 
 SELECT * FROM Properties
-LEFT JOIN Reviews ON Properties.id = Reviews.property_id;
+LEFT JOIN Reviews ON Properties.id = Reviews.property_id
+ORDER BY Properties.id;
 
 SELECT * FROM Users
 FULL OUTER JOIN Bookings ON Users.id = Bookings.user_id;
