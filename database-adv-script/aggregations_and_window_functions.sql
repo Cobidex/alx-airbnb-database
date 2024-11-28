55169f0a786b1eb604458162e13d57649baefe9e
Stephen <stevobialor@gmail.com~>
2024-11-28 21:23:52 +0100

    add: query to rank properties using window function RANK

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,12 +3,12 @@ GROUP BY b.user_id;
 
 SELECT
   b.property_id,
-  RANK() OVER (ORDER BY COUNT(*) DESC),
+  RANK() OVER (ORDER BY COUNT(*) DESC)
 FROM Booking b
 GROUP BY b.property_id;
 
 SELECT
   b.property_id,
-  ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC),
+  ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC)
 FROM Booking b
 GROUP BY b.property_id;
