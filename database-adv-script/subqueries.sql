312b9382b8ad448f544eef29bf09b3f5a6b4a009
Stephen <stevobialor@gmail.com~>
2024-11-28 16:17:59 +0100

    add: correlated query

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -5,3 +5,9 @@ WHERE p.id IN
     GROUP BY r.property_id
     HAVING AVG(r.rating) > 4
     );
+
+SELECT * FROM User u
+WHERE (
+    SELECT COUNT(*) FROM Booking b
+    WHERE b.user_id = u.id
+    ) > 3;
