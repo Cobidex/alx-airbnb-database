992440809584e3b11dd17ad5ab0e053fe92347a1
Stephen <stevobialor@gmail.com~>
2024-11-28 16:19:38 +0100

    fix: entity name

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,7 +1,7 @@
-SELECT * FROM Properties p
+SELECT * FROM Property p
 WHERE p.id IN 
   (
-    SELECT property_id FROM Review r
+    SELECT r.property_id FROM Review r
     GROUP BY r.property_id
     HAVING AVG(r.rating) > 4
     );
