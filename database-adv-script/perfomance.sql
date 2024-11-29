d6a77411c198fb3524bebc5d0b7913a17795852a
Stephen <stevobialor@gmail.com~>
2024-11-29 21:59:58 +0100

    optimize query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -2,6 +2,7 @@ EXPLAIN SELECT * FROM booking b
 JOIN user u ON b.user_id = u.id
 JOIN property p ON b.property_id = p.id
 JOIN payment pay ON b.id = pay.booking_id
+WHERE b.id = '1' AND u.id = '1' AND p.id = '1' AND pay.id = '1';
 
 CREATE INDEX user_id_index ON user (id);
 CREATE INDEX property_id_index ON property (id);
@@ -12,3 +13,4 @@ EXPLAIN SELECT * FROM booking b
 JOIN user u ON b.user_id = u.id
 JOIN property p ON b.property_id = p.id
 JOIN payment pay ON b.id = pay.booking_id
+WHERE b.id = '1' AND u.id = '1' AND p.id = '1' AND pay.id = '1';
